fadbac5c5b7037003849eb03eac7293611240eb9
yinjunjie <yinjunjie@gaodun.com>
2025-10-24 01:49:15 +0800

    1024-1

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8089',
+        target: 'http://learningservice.zeabur.internal:8080',
         changeOrigin: true,
         secure: false
       }
