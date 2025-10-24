1f526720a622c80c23e0418168dfab299668ae9c
yinjunjie <yinjunjie@gaodun.com>
2025-10-24 22:31:41 +0800

    11

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://pm-feel666.gaodun.com',
+        target: 'https://pm-feel666service.gaodun.com',
         changeOrigin: true,
         secure: false
       }
@@ -36,7 +36,7 @@ export default defineConfig({
   preview: {
     proxy: {
       '/api': {
-        target: 'https://pm-feel666.gaodun.com',
+        target: 'https://pm-feel666service.gaodun.com',
         changeOrigin: true,
         secure: false
       }
